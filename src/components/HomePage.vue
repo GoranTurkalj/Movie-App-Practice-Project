69a1a79ca8971aaec2fda470b7c2d4972c6def8a
GoranTurkalj <turkalj.goran@yahoo.com>
2020-09-18 13:58:54 +0200

    typo

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -22,9 +22,9 @@
         </p>
         <p slot="info-tip-2" v-show="!getUser.name">
           To use the watchlist, just
-          <router-link to="/signin">sign-in</router-link>to your account.
+          <router-link to="/signin">sign-in </router-link>to your account.
           <br />If you don't have an account, create one by
-          <router-link to="/signup">signing-up</router-link>now.
+          <router-link to="/signup">signing-up</router-link> now.
         </p>
       </general-info>
       <movie-list v-else :titlesArray="getResultsList" :displayTitle="showFullTitle"></movie-list>
